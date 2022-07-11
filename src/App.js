a62155be652b8a9b802dce402ea7c9c829e3fc3b
Nicolas Julian Torres <71136123+JulianTorres1@users.noreply.github.com>
2022-07-10 20:13:49 -0500

    init commit
    just starting the project

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
   Code,
   Grid,
   theme,
+  Heading,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
@@ -19,19 +20,9 @@ function App() {
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+            <Heading as='h1' size={'3xl'} >
+              Habits Aplication!
+            </Heading>
           </VStack>
         </Grid>
       </Box>
